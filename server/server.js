1776f3bfa559804cae8d4682c386977d55ba774f
kevin-murithi <murithikevin54@gmail.com>
2025-10-29 19:38:08 +0300

    Server CORS Update

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser());
 const corsOptions = {
-  origin: ['http://localhost:5174', 'http://localhost:5173'], 
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'https://clima-score.vercel.app'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
